b304cab50ff53d85db401d9421bf26ee289774d0
daniel-chenweiyu <ji3g4jom3@gmail.com>
2015-12-12 15:29:51 +0800

    建立檔案

---
+++ HomePractice/HomePractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomePractice.Startup))]
+namespace HomePractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
